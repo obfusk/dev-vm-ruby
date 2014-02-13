982719ba279a3ec915f4d9c68f7aa59b122f98d3
Felix C. Stegerman <flx@obfusk.net>
2014-02-13 08:23:55 +0100

    ansible: user!, ...

--- ansible.sh
+++ ansible.sh
@@ -4,4 +4,4 @@ set -xe
 export LC_ALL=C
 
 rsync -e 'ssh -F .ssh-config' -av --delete ansible/ default:ansible/
-vagrant ssh -c 'cd ~/ansible && sudo ansible-playbook -v -i hosts site.yml'
+vagrant ssh -c 'cd ~/ansible && ansible-playbook -v -i hosts site.yml'
