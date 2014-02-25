8e8f3f0180efcea4eaf5bda82885af0d9c25e327
Felix C. Stegerman <flx@obfusk.net>
2014-02-25 22:58:36 +0100

    only use backports for precise

--- provision.sh
+++ provision.sh
@@ -3,12 +3,14 @@ set -xe
 : ${RELEASE:=precise}
 
 if ! dpkg -s ansible >/dev/null 2>&1; then
-  sed 's!^    !!' > /etc/apt/sources.list.d/backports.list <<____END
-    deb     http://archive.ubuntu.com/ubuntu $RELEASE-backports \
-      main restricted universe
-    deb-src http://archive.ubuntu.com/ubuntu $RELEASE-backports \
-      main restricted universe
-____END
+  if [ "$RELEASE" = precise ]; then
+    sed 's!^    !!' > /etc/apt/sources.list.d/backports.list <<______END
+      deb     http://archive.ubuntu.com/ubuntu $RELEASE-backports \
+        main restricted universe
+      deb-src http://archive.ubuntu.com/ubuntu $RELEASE-backports \
+        main restricted universe
+______END
+  fi
 
   aptitude update
   aptitude -y -t $RELEASE-backports install ansible
