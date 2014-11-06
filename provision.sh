c6649e299fde71d245a2ec4180db0e2321023d90
Felix C. Stegerman <flx@obfusk.net>
2014-11-06 16:17:24 +0100

    + scala; ...

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@ set -xe
 : ${RELEASE:=trusty}
 
 if ! dpkg -s ansible >/dev/null 2>&1; then
-  apt-add-repository ppa:ansible/ansible
+  apt-add-repository ppa:ansible/ansible -y
   aptitude update
   aptitude -y install ansible
 fi
